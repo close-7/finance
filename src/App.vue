796e4309a9870d15617b81659d0d5cc6341a35a8
close-7 <531210222@qq.com>
2020-11-09 21:52:40 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+// import Home from './pages/home'
+
+export default {
+  name: 'App',
+  components: {
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div :class="$style.home">home </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" module>
+@import '../assets/css/rest.scss';
+  .home{
+    color: red;
+    font-size: 80px;
+  }
+</style>
